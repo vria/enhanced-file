e9027c55ad6d6bf905df77532b67a0e2b4f35099
riabchenko <riabchenko.vlad@gmail.com>
2016-05-05 16:33:17 +0200

    file type and template blocks

---
+++ src/Form/EnhancedFileType.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace VRia\Bundle\EnhancedFileBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EnhancedFileType extends AbstractType
+{
+    /**
+     * Builds the form.
+     *
+     * This method is called for each type in the hierarchy starting from the
+     * top most type. Type extensions can further modify the form.
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array                $options The options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('fileName', HiddenType::class)
+            ->add('file', FileType::class, array(
+                'label' => false,
+                'multiple' => $options['multiple']
+            ))
+            ->addModelTransformer(new StringToEnhancedFileTransformer($options['directory_path']))
+            ->addEventListener(FormEvents::PRE_SUBMIT, function(FormEvent $event) use ($options) {
+                $data = $event->getData();
+
+                if ($data['file'] instanceof UploadedFile) {
+                    $fileName = md5(uniqid()) . '.' . $data['file']->guessExtension();
+
+                    $data['file']->move($options['directory_path'], $fileName);
+                    $data['fileName'] = $fileName;
+
+                    $event->setData($data);
+                }
+            });
+    }
+
+    /**
+     * Finishes the form view.
+     *
+     * This method gets called for each type in the hierarchy starting from the
+     * top most type. Type extensions can further modify the view.
+     *
+     * When this method is called, views of the form's children have already
+     * been built and finished and can be accessed. You should only implement
+     * such logic in this method that actually accesses child views. For everything
+     * else you are recommended to implement {@link buildView()} instead.
+     *
+     * @see FormTypeExtensionInterface::finishView()
+     *
+     * @param FormView      $view    The view
+     * @param FormInterface $form    The form
+     * @param array         $options The options
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $fileName = $form->get('fileName')->getData();
+        $view->vars['download_url'] = $fileName ? $options['public_directory_path'] . $fileName : null;
+
+        $view->children['file']->vars = array_replace($view->vars, $view->children['file']->vars);
+    }
+
+
+    /**
+     * Configures the options for this type.
+     *
+     * @param OptionsResolver $resolver The resolver for the options
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'compound' => true,
+            'multiple' => false,
+        ));
+
+        $resolver->setRequired('directory_path');
+        $resolver->setRequired('public_directory_path');
+    }
+
+    /**
+     * Returns the prefix of the template block name for this type.
+     *
+     * The block prefix defaults to the underscored short class name with
+     * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
+     *
+     * @return string The prefix of the template block name
+     */
+    public function getBlockPrefix()
+    {
+        return 'enhanced_file';
+    }
+}
